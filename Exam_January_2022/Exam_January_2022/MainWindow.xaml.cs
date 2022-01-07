e7a5ff60497a3e1bac4afdcfa7169b16ed8ddf4b
StevenK418 <50826648+StevenK418@users.noreply.github.com>
2022-01-07 16:47:15 +0000

    => Initial Commit of project to repository.

---
+++ Exam_January_2022/Exam_January_2022/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Name:                Steven Kelly
+ * StudentID:           S00200293
+ * Date:                07/01/2022
+ * Lab:                 Exam_January_2022
+ * Description:         "Club Members"
+ * Developer notes:      
+ * Github: 
+ *                      Clone: "https://github.com/StevenK418/OOP_Exam_January_2022.git"
+ *                      View Online: "https://github.com/StevenK418/OOP_Exam_January_2022"
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exam_January_2022
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+       
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+          
+        }
+    }
+}
